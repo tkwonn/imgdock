daa3e6e50ce4dc64cbcc48b69c31a717ec4608de
Taesok Kwon <kwontaesok1997@gmail.com>
2025-02-02 21:06:09 -0800

    Update workflow

--- src/Helpers/Settings.php
+++ src/Helpers/Settings.php
@@ -6,16 +6,14 @@ use Exception;
 
 class Settings
 {
-    private const ENV_PATH = '.env';
-
-    public static function env(string $pair): string
+    public static function env(string $key): string
     {
-        $config = parse_ini_file(dirname(__FILE__, 3) . '/' . self::ENV_PATH);
 
-        if ($config === false) {
+        $val = getenv($key);
+        if ($val === false) {
             throw new Exception('Failed to read and parse .env file');
         }
 
-        return $config[$pair];
+        return $val;
     }
 }
