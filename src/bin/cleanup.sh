48c8feda1bedeec3745c078876bf12156b95cdd7
Taesok Kwon <kwontaesok1997@gmail.com>
2025-02-02 19:42:02 -0800

    Update workflow

--- src/bin/cleanup.sh
+++ src/bin/cleanup.sh
@@ -4,11 +4,16 @@ set -e
 APP_DIR="/home/ubuntu/web/imgdock"
 
 cd "$APP_DIR"
-echo "[INFO] Stopping running containers..."
-sudo docker stop $(sudo docker ps -q) || true
+echo "[INFO] Attempting to stop running containers..."
+CONTAINERS=$(sudo docker ps -q)
+if [ -n "$CONTAINERS" ]; then
+  sudo docker stop $CONTAINERS
+else
+  echo "[INFO] No running containers."
+fi
 
 echo "[INFO] docker-compose down..."
-sudo docker compose -f compose-prod.yml down || true
+sudo docker-compose -f compose-prod.yml down || true
 
 echo "[INFO] Removing unused images, containers, networks..."
 sudo docker system prune -af
